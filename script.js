636ba6b6f73dcd17d016e547b31452a04f40a822
Tia <tia@hismileteeth.com>
2022-05-23 11:47:12 +1000

    init commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// When the + icon is clicked - we want this to render a new task template with input fields to fill in for the task
+
+// After the new task template is rendered, we want the user to be able to submit the new fields they have filled in for the new task - submit button creates new task
+
+// I want to have all tasks render in a list
+
+// if a task is clicked, i want that task to open and show me all details of the task and i want the task to close if the x in the left corner is clicked
+
+// if a task is open, and the user clicks the edit icon in the top right, we want the task to show the fields as inputs with the current data in them but edit-able with a 'save changes' button that will replace the current data
+
+// if the user clicks the trash can icon, we want the task to be deleted and take the user back to the list of tasks
